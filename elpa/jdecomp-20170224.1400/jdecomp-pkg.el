29c3959247e3cc7027b55d6e47c55f25fc53e202
Jon Distad <jon.distad@gmail.com>
2017-07-31 10:27:13 -0400

    update ensime

--- elpa/jdecomp-20170224.1400/jdecomp-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "jdecomp" "20170224.1400" "Interface to Java decompilers" '((emacs "24.5")) :commit "692866abc83deedce62be8d6040cf24dda7fb7a8" :url "https://github.com/xiongtx/jdecomp" :keywords '("decompile" "java" "languages" "tools"))
